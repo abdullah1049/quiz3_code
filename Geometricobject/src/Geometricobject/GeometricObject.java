d73f99aebc3a133b0a11956aa6c4eda9a87a0703
LENOVO <LENOVO@128.4.201.249>
2014-09-28 17:07:41 -0700

    quiz3 done

---
+++ Geometricobject/src/Geometricobject/GeometricObject.java
@@ -0,0 +1,20 @@
+// Abdullah AL-Sinaidi
+package Geometricobject;
+
+public abstract class GeometricObject{
+	
+	
+	// default constructor
+	protected GeometricObject(){
+		
+	}
+	
+	public abstract double getArea();
+	public abstract double getPerimeter();
+	public abstract String toString();
+	
+}
+	
+
+	
+
---
+++ Geometricobject/src/Geometricobject/test_triangle.java
@@ -0,0 +1,14 @@
+package Geometricobject;
+
+public class test_triangle {
+		public static void main(String[] args){
+			
+			triangle Triangle = new triangle(1.0,1.0,1.0);
+			
+		
+			System.out.println(Triangle.toString());
+
+		}
+	}
+
+
